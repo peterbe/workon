09755dde7bd2a3dc716948c4be03470d01fc4356
Peter Bengtsson <mail@peterbe.com>
2018-07-11 11:26:27 -0400

    correct frame-src csp

--- bin/insert_csp.js
+++ bin/insert_csp.js
@@ -4,7 +4,8 @@ const spawn = require("child_process").spawnSync;
 
 const CSP = `
 default-src 'none';
-connect-src  kinto.workon.app peterbecom.auth0.com;
+connect-src kinto.workon.app peterbecom.auth0.com;
+frame-src peterbecom.auth0.com;
 img-src 'self' avatars2.githubusercontent.com https://*.googleusercontent.com;
 script-src 'self';
 style-src 'self' 'unsafe-inline';
