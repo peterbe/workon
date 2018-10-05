223dac8c62dda95c313bc57f6a9adc5362290a46
Peter Bengtsson <mail@peterbe.com>
2018-10-04 20:21:45 -0400

    new service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./font-face.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+serviceWorker.register();
