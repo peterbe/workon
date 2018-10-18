2d4ba682778b6197c3d328bb07a6a7800c77ba4b
Peter Bengtsson <mail@peterbe.com>
2018-10-17 21:03:01 -0400

    Updates

---
+++ precache-manifest.bf43dd12b62b9968c0a871c21969c2bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "376e03b5e32e391e8611",
+    "url": "/static/js/main.376e03b5.chunk.js"
+  },
+  {
+    "revision": "f23d6fa574c5b6795591",
+    "url": "/static/js/1.f23d6fa5.chunk.js"
+  },
+  {
+    "revision": "376e03b5e32e391e8611",
+    "url": "/static/css/main.cfe877fa.chunk.css"
+  },
+  {
+    "revision": "f23d6fa574c5b6795591",
+    "url": "/static/css/1.af866cd5.chunk.css"
+  },
+  {
+    "revision": "25cccadcec7caf6ebe349e612fd761a6",
+    "url": "/index.html"
+  }
+];
