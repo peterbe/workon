1f6b6856442a8fb98775c8bf120ab0aa89303f37
Peter Bengtsson <mail@peterbe.com>
2018-10-18 14:01:18 -0400

    no inline scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-REACT_APP_KINTO_URL=https://kinto.workon.app/v1 yarn run build
+INLINE_RUNTIME_CHUNK=false REACT_APP_KINTO_URL=https://kinto.workon.app/v1 yarn run build
 
-./bin/uninline_scripts.js build/index.html build/asset-manifest.json
+# ./bin/uninline_scripts.js build/index.html build/asset-manifest.json
 
 ./bin/insert_csp.js build/index.html
 
