720fc9ebbb79bbcb34e960470f1aef4d0a51058a
Peter Bengtsson <mail@peterbe.com>
2018-10-17 20:36:17 -0400

    Updates

---
+++ precache-manifest.a361d2b784f4280503f329700dd17917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c417d5a0e57066dcb00a",
+    "url": "/static/js/main.c417d5a0.chunk.js"
+  },
+  {
+    "revision": "14525a3020095d80b55a",
+    "url": "/static/js/1.14525a30.chunk.js"
+  },
+  {
+    "revision": "c417d5a0e57066dcb00a",
+    "url": "/static/css/main.cfa25cca.chunk.css"
+  },
+  {
+    "revision": "14525a3020095d80b55a",
+    "url": "/static/css/1.8c4cde56.chunk.css"
+  },
+  {
+    "revision": "fd0c96d8d6d2a2161c850e57100db7c9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98cbba869d2d06c89e8ded537de40a94.js"
+  "/precache-manifest.a361d2b784f4280503f329700dd17917.js"
 );
 
 workbox.clientsClaim();
