76b99c834d51dd1dda1066df168bc7ce53f76d4b
Peter Bengtsson <mail@peterbe.com>
2019-01-30 13:39:22 -0500

    better footer

--- bin/insert_version.js
+++ bin/insert_version.js
@@ -5,14 +5,6 @@ const versionFile = process.argv[2];
 if (!versionFile) throw new Error("missing file argument");
 const versionString = fs.readFileSync(versionFile, "utf8");
 const data = JSON.parse(versionString);
-// const child_process = require("child_process");
-
-// const data = {};
-
-// data.sha = child_process
-//   .execSync("git rev-parse HEAD")
-//   .toString()
-//   .trim();
 
 const htmlFile = process.argv[3];
 if (!htmlFile) throw new Error("missing file argument");
@@ -22,13 +14,10 @@ let tag = '<div id="_version" ';
 const keeps = ["commit", "date"];
 Object.entries(data).forEach(([key, value]) => {
   if (keeps.includes(key)) {
-    // console.log("KEY", key, "VALUE", value);
     tag += `data-${key}="${value}" `;
   }
 });
 tag = tag.trim() + "/>";
 
-const newHtml = html.replace("</div>", `</div>${tag}`);
-// fs.writeFileSync(htmlFile + ".backup", newHtml, "utf8");
+const newHtml = html.replace(/<div id="_version" [^>]+>/, tag);
 fs.writeFileSync(htmlFile, newHtml, "utf8");
-// console.log(tag);
