975c6ee86a9eb8cd732295782cd9a449402673b5
Peter Bengtsson <mail@peterbe.com>
2019-04-23 14:24:02 -0400

    fix for build step

---
+++ bin/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+INLINE_RUNTIME_CHUNK=false REACT_APP_KINTO_URL=https://kinto.workon.app/v1 yarn run build
+
+./bin/insert_csp.js build/index.html
+
+./bin/update_version.js > build/version.json
+
+./bin/insert_version.js build/version.json build/index.html
+
+./bin/move_print_css_last.js build/index.html
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-INLINE_RUNTIME_CHUNK=false REACT_APP_KINTO_URL=https://kinto.workon.app/v1 yarn run build
-
-# ./bin/uninline_scripts.js build/index.html build/asset-manifest.json
-
-./bin/insert_csp.js build/index.html
-
-./bin/update_version.js > build/version.json
-
-./bin/insert_version.js build/version.json build/index.html
-
-./bin/move_print_css_last.js build/index.html
+./bin/build.sh
 
 gh-pages --add --dist build/
