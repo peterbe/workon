6edba7deca774cae4e9b64ef195362d948c7ef21
Peter Bengtsson <mail@peterbe.com>
2019-06-06 10:09:53 -0400

    deploy manually

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,15 @@ set -eo pipefail
 
 ./bin/build.sh
 
-gh-pages --add --dist build/
+rm -fr build.zip
+pushd build
+time zopfli -i500 index.html
+time zopfli -i500 static/js/*.js
+time zopfli -i500 static/css/*.css
+time brotli index.html
+time brotli static/js/*.js
+time brotli static/css/*.css
+popd
+apack build.zip build
+
+scp build.zip django@192.34.57.223:/home/django/workon/
